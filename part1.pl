f9508e5f18b38c5f4bb2c8410f28e754fbfff6db
yagyanshbhatia <yagyansh1999@gmail.com>
2019-09-28 23:03:15 +0530

    Assignment part 1 Complete.

---
+++ part1.pl
@@ -0,0 +1,5 @@
+decode([],1).
+decode([H|Tail], N):- char_code(H, HH), HH is 49, [_|Rest]=Tail, !, decode(Rest,M), decode(Tail,O), N is M+O;
+        char_code(H, HH), HH is 50, [I|Rest]=Tail, char_code(I, II), II < 55, !, decode(Rest,M), decode(Tail,O), N is M+O;
+        decode(Tail, N).
+evaluate(S):- string_chars(S, A), decode(A, Ans), write(Ans).
