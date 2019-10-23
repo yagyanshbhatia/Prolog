c42dcb6a961fb7776d2d8bcdf26d9477338eaca0
yagyanshbhatia <yagyansh1999@gmail.com>
2019-10-24 01:14:29 +0530

    Singleton variable removed

--- part1.pl
+++ part1.pl
@@ -1,5 +1,5 @@
 decode([],1).
-decode(['0'|Tail], 0).
+decode(['0'|_], 0).
 decode([H|Tail], N):- char_code(H, HH), HH is 49, [_|Rest]=Tail, !, decode(Rest,M), decode(Tail,O), N is M+O;
         char_code(H, HH), HH is 50, [I|Rest]=Tail, char_code(I, II), II < 55, !, decode(Rest,M), decode(Tail,O), N is M+O;
         decode(Tail, N).
