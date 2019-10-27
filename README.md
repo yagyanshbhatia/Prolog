12dc60e8ad7e410eced63e5ba17d9fa6f732faa8
yagyanshbhatia <yagyansh1999@gmail.com>
2019-10-27 10:50:57 +0530

    third complete

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Programming in Prolog, as part of school curriculum
 <code>swipl</code>
 ## run .pl file
 <code>['path_to_the_file'].</code>
+
+Use <code>aggregate_all(count, allPossible(X), Count).</code> to count all possible paths in part 3. 
--- part3.pl
+++ part3.pl
@@ -1,6 +1,6 @@
-allPath(A, B, [A, B]) :- edge(A, B, _).
-allPath(A, B, [A | Tail]) :- [X | _] = Tail, edge(A, X, _), allPath(X, B, Tail). 
-allPossible(X) :- member(A, [g1, g2, g3, g4]), allPath(A, g17, X).
+allPath(A, B, [A, B], L) :- not(member(B, L)), edge(A, B, _).
+allPath(A, B, [A | Tail], L) :- [X | _] = Tail, edge(A, X, _), not(member(X, L)), append([X], L, Z), allPath(X, B, Tail, Z).
+allPossible(X) :- member(A, [g1, g2, g3, g4]), allPath(A, g17, X, [A]).
 
 
 pathLength([A,B], Length) :- edge(A,B,Length).
@@ -15,43 +15,83 @@ validPath([A|Tail], X) :- [B|_] = Tail, edge(A, B, _), validPath(Tail, X).
 validPath([A|Tail], X) :- [B|_] = Tail, edge(B, A, _), validPath(Tail, X).
 
 
-edge(g1,g5,4).
-edge(g2,g5,6).
-edge(g3,g5,8).
-edge(g4,g5,9).
-edge(g1,g6,10).
-edge(g2,g6,9).
-edge(g3,g6,3).
-edge(g4,g6,5).
-edge(g5,g7,3).
-edge(g5,g10,4).
-edge(g5,g11,6).
-edge(g5,g12,7).
-edge(g5,g6,7).
-edge(g5,g8,9).
-edge(g6,g8,2).
-edge(g6,g12,3).
-edge(g6,g11,5).
-edge(g6,g10,9).
-edge(g6,g7,10).
-edge(g7,g10,2).
-edge(g7,g11,5).
-edge(g7,g12,7).
-edge(g7,g8,10).
-edge(g8,g9,3).
-edge(g8,g12,3).
-edge(g8,g11,4).
-edge(g8,g10,8).
-edge(g10,g15,5).
-edge(g10,g11,2).
-edge(g10,g12,5).
-edge(g11,g15,4).
-edge(g11,g13,5).
-edge(g11,g12,4).
-edge(g12,g13,7).
-edge(g12,g14,8).
-edge(g15,g13,3).
-edge(g13,g14,4).
-edge(g14,g17,5).
-edge(g14,g18,4).
-edge(g17,g18,8).
+edge(g1,g5,4). 
+edge(g5,g1,4).
+edge(g2,g5,6). 
+edge(g5,g2,6).
+edge(g3,g5,8). 
+edge(g5,g3,8).
+edge(g4,g5,9). 
+edge(g5,g4,9).
+edge(g1,g6,10). 
+edge(g6,g1,10).
+edge(g2,g6,9). 
+edge(g6,g2,9).
+edge(g3,g6,3). 
+edge(g6,g3,3).
+edge(g4,g6,5). 
+edge(g6,g4,5).
+edge(g5,g7,3). 
+edge(g7,g5,3).
+edge(g5,g10,4). 
+edge(g10,g5,4).
+edge(g5,g11,6). 
+edge(g11,g5,6).
+edge(g5,g12,7). 
+edge(g12,g5,7).
+edge(g5,g6,7). 
+edge(g6,g5,7).
+edge(g5,g8,9). 
+edge(g8,g5,9).
+edge(g6,g8,2). 
+edge(g8,g6,2).
+edge(g6,g12,3). 
+edge(g12,g6,3).
+edge(g6,g11,5). 
+edge(g11,g6,5).
+edge(g6,g10,9). 
+edge(g10,g6,9).
+edge(g6,g7,10). 
+edge(g7,g6,10).
+edge(g7,g10,2). 
+edge(g10,g7,2).
+edge(g7,g11,5). 
+edge(g11,g7,5).
+edge(g7,g12,7). 
+edge(g12,g7,7).
+edge(g7,g8,10). 
+edge(g8,g7,10).
+edge(g8,g9,3). 
+edge(g9,g8,3).
+edge(g8,g12,3). 
+edge(g12,g8,3).
+edge(g8,g11,4). 
+edge(g11,g8,4).
+edge(g8,g10,8). 
+edge(g10,g8,8).
+edge(g10,g15,5). 
+edge(g15,g10,5).
+edge(g10,g11,2). 
+edge(g11,g10,2).
+edge(g10,g12,5). 
+edge(g12,g10,5).
+edge(g11,g15,4). 
+edge(g15,g11,4).
+edge(g11,g13,5). 
+edge(g13,g11,5).
+edge(g11,g12,4). 
+edge(g12,g11,4).
+edge(g12,g13,7). 
+edge(g13,g12,7).
+edge(g12,g14,8). 
+edge(g14,g12,8).
+edge(g15,g13,3). 
+edge(g13,g15,3).
+edge(g13,g14,4). 
+edge(g14,g13,4).
+edge(g14,g17,5). 
+edge(g17,g14,5).
+edge(g14,g18,4). 
+edge(g18,g14,4).
+edge(g17,g18,8). 
+edge(g18,g17,8).
