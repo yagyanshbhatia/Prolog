c4f77ea7a12b3567a85c16ff596162e86ef94cb9
yagyanshbhatia <yagyansh1999@gmail.com>
2019-10-27 10:55:53 +0530

    minor bug fix in part2

--- part1.pl
+++ part1.pl
@@ -1,6 +1,6 @@
 decode([],1).
-decode(['0'|_], 0).
+decode(['0'|_], 0) :- writeln("Invalid String ").
 decode([H|Tail], N):- char_code(H, HH), HH is 49, [_|Rest]=Tail, !, decode(Rest,M), decode(Tail,O), N is M+O;
         char_code(H, HH), HH is 50, [I|Rest]=Tail, char_code(I, II), II < 55, !, decode(Rest,M), decode(Tail,O), N is M+O;
-        decode(Tail, N).
-evaluate(S):- string_chars(S, A), decode(A, Ans), write(Ans).
+        char_code(H, HH), not(HH is 48), decode(Tail, N).
+evaluate(S):- string_chars(S, A), decode(A, Ans), write("Number of ways to decode: "), write(Ans).
